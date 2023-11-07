9b1e3399832a69a07349504c1c1904cbd575f0ab
Anton.Labkovich <anton.labkovich@triplenext.com>
2023-11-07 17:59:21 +0300

    Board CRUD

---
+++ src/app/api/boards/dto.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const createBoardDto = z.object({
+    title: z.string().min(1).max(20),
+})
+
+export const updateBoardDto = createBoardDto.partial()
+
---
+++ src/app/api/boards/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse} from 'next/server'
+import { createBoardDto } from './dto';
+import { prisma } from '@/app/core/prisma';
+
+export const GET = async (req: Request) => {
+    const boards = await prisma.boards.findMany();
+
+    return NextResponse.json(boards); 
+}
+
+export const POST = async (req: Request) => {
+    const bodyRaw = await req.json();
+    const validateBody = createBoardDto.safeParse(bodyRaw);
+
+    if(!validateBody.success) {
+        return NextResponse.json(validateBody.error.issues, {
+            status: 400,
+        })
+    }
+
+    const { title } = validateBody.data;
+
+    const newBoard = await prisma.boards.create({
+        data: {
+            title,
+        }
+    })
+
+    return NextResponse.json(newBoard);
+} 
+
