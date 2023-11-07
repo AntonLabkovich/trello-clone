2a538610a463e98ec25eb371fbbb05ca4529850e
Anton.Labkovich <anton.labkovich@triplenext.com>
2023-11-07 21:39:28 +0300

    board page basic layout

--- src/app/components/navbar.components.tsx
+++ src/app/components/navbar.components.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { UserDropdown } from "./user-dropdown.component";
 
-export const NavBar = () => <nav className="bg-white border-gray-200 dark:bg-gray-900">
-<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+export const NavBar = () => <nav className="mb-10 bg-white border-gray-200 dark:bg-gray-800">
+<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
   <Link href="/" className="flex items-center">
     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Trello</span>
   </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { NavBar } from "./components";
+import { BoardCard } from "./components/board-card.component";
 
 export default function Home() {
+  
   return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
-    <NavBar/>
+        <BoardCard id={'1'} title={'task'}/>
+        <BoardCard id={'1'} title={'task1'}/>
+        <BoardCard id={'1'} title={'task2'}/>
+
+        
+      </div>
+    </div>
 
   )
 }
